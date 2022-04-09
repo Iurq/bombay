b57b2000fd7dc2146d900055cada445eef866c8d
Gurseerit <66850320+Iurq@users.noreply.github.com>
2022-04-09 17:08:11 +0100

    init

---
+++ python/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
+
+@app.route('/')
+def index():
+    return render_template("index/index.html")
+@app.route('/why')
+def why():
+    return render_template("why/why.html")
+@app.route('/nearby')
+def nearby():
+    return render_template("nearby/nearby.html")
+@app.route('/contact')
+def contact():
+    return render_template("contact/contact.html")
+
+
+if __name__ == "__main__" :
+    app.run()
